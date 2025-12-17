db2be7767cd96c85511fd256e53d7b4ef112cc59
Carolina Vasco <150808768+Carolina-Vasco1@users.noreply.github.com>
2025-12-16 19:13:17 -0500

    Migrate SQL from MySQL to PostgreSQL syntax

--- bd/init.sql
+++ bd/init.sql
@@ -1,10 +1,7 @@
-CREATE DATABASE holamundo;
-
-USE holamundo;
-
 CREATE TABLE mensajes (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  texto VARCHAR(100)
+id SERIAL PRIMARY KEY,
+texto VARCHAR(100)
 );
 
-INSERT INTO mensajes (texto) VALUES ('Hola Mundo desde la Base de Datos');
+
+INSERT INTO mensajes (texto) VALUES ('Hola Mundo desde PostgreSQL');
